ff8cca53b673b2cf77f5b821cf6b413253e93b45
Mikhail Topal <wallguard313@gmail.com>
2019-12-17 12:36:16 +0300

    first step

---
+++ app/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+const hbsConfig = exphbs.create({
+  layoutsDir: path.join(__dirname, 'views/layouts/'),
+  defaultLayout: 'main',
+  extname: '.hbs',
+});
+
+app.engine('.hbs', hbsConfig.engine);
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+  res.render('home', { home: true });
+});
+
+app.get('/features', (req, res) => {
+  res.render('features', { features: true });
+});
+
+app.get('/news', (req, res) => {
+  res.render('news', { news: true });
+});
+
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require('./app/app.js');
+const PORT = 3002;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+});
