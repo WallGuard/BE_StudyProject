55797a9a4c056b3874b6f0fd327268d7de4065a2
Wallguard <wallguard@localhost.localdomain>
2020-03-02 12:21:41 +0300

    work triggers

--- tables/migrations/00000000000000-trigger-LikesCount.js
+++ tables/migrations/00000000000000-trigger-LikesCount.js
@@ -15,7 +15,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190213113234-trigger-LikesCount-up.sql');
+  var filePath = path.join(__dirname, 'sqls', 'trigger-LikesCount-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -30,7 +30,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190213113234-trigger-LikesCount-down.sql');
+  var filePath = path.join(__dirname, 'sqls', 'trigger-LikesCount-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -43,4 +43,3 @@ exports.down = function(db) {
     return db.runSql(data);
   });
 };
-
--- tables/migrations/00000000000000-trigger-NotesCount.js
+++ tables/migrations/00000000000000-trigger-NotesCount.js
@@ -15,7 +15,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190213112506-trigger-NotesCount-up.sql');
+  var filePath = path.join(__dirname, 'sqls', 'trigger-NotesCount-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
