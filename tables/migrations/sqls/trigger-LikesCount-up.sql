dd058499e604509eee1afbb079457cb40cd65790
Wallguard <wallguard@localhost.localdomain>
2020-03-03 10:21:16 +0300

    triggers

--- tables/migrations/sqls/trigger-LikesCount-up.sql
+++ tables/migrations/sqls/trigger-LikesCount-up.sql
@@ -1,11 +1,6 @@
--- STEP 1
--- ADD COLUMN "likes_count" to table "Notes"
 ALTER TABLE "Notes"
   ADD COLUMN likes_count INTEGER DEFAULT 0;
 
--- STEP 2
--- CREATE TRIGGER for INCREASE by 1 column "likes_count"
--- in table "Notes" after adding new record to table "Likes"
 CREATE TRIGGER IF NOT EXISTS "Notes_incr_LikesCount"
   AFTER INSERT ON "Likes"
   FOR EACH ROW BEGIN
@@ -16,9 +11,6 @@ CREATE TRIGGER IF NOT EXISTS "Notes_incr_LikesCount"
 
   END;
 
--- STEP 3
--- CREATE TRIGGER for DECREASE by 1 column "likes_count"
--- in table "Notes" after deleting record from table "Likes"
 CREATE TRIGGER IF NOT EXISTS "Notes_decr_LikesCount"
   AFTER DELETE ON "Likes"
   FOR EACH ROW BEGIN
--- tables/migrations/sqls/trigger-NotesCount-up.sql
+++ tables/migrations/sqls/trigger-NotesCount-up.sql
@@ -1,11 +1,6 @@
--- STEP 1
--- ADD COLUMN "notes_count" to table "Users"
 ALTER TABLE "Users"
   ADD COLUMN notes_count INTEGER DEFAULT 0;
 
--- STEP 2
--- CREATE TRIGGER for INCREASE by 1 column "notes_count"
--- in table "Users" after adding new record to table "Notes"
 CREATE TRIGGER IF NOT EXISTS "Users_incr_NotesCount"
   AFTER INSERT ON "Notes"
   FOR EACH ROW BEGIN
@@ -16,9 +11,6 @@ CREATE TRIGGER IF NOT EXISTS "Users_incr_NotesCount"
 
   END;
 
--- STEP 3
--- CREATE TRIGGER for DECREASE by 1 column "notes_count"
--- in table "Users" after deleting record from table "Notes"
 CREATE TRIGGER IF NOT EXISTS "Users_decr_NotesCount"
   AFTER DELETE ON "Notes"
   FOR EACH ROW BEGIN
