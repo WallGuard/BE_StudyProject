5076e62296cdbdb4100d9f090ead3a227f6a929a
Wallguard <wallguard@localhost.localdomain>
2020-03-04 08:49:04 +0300

    fix migrations

--- tables/migrations/00000000000005-tags.js
+++ tables/migrations/00000000000005-tags.js
@@ -1,43 +1,7 @@
-const fs = require('fs');
 const path = require('path');
-const filePath_up = path.join(__dirname, 'sqls', 'table_Tags-up.sql');
-const filePath_down = path.join(__dirname, 'sqls', 'table_Tags-down.sql');
+const boilerplate = require('db-migrate-boilerplate');
 
-exports.setup = function (options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-  Promise = options.Promise;
-};
-
-exports.up = function (db) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(filePath_up, { encoding: 'utf-8' }, function (err, data) {
-      if (err) return reject(err);
-      console.log('received data: ' + data);
-
-      resolve(data);
-    });
-  })
-    .then(function (data) {
-      return db.runSql(data);
-    });
-};
-
-exports.down = function (db) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(filePath_down, { encoding: 'utf-8' }, function (err, data) {
-      if (err) return reject(err);
-      console.log('received data: ' + data);
-
-      resolve(data);
-    });
-  })
-    .then(function (data) {
-      return db.runSql(data);
-    });
-};
-
-exports._meta = {
-  'version': 1
-};
+module.exports = boilerplate ({
+  upPath: path.join(__dirname, 'sqls', 'table_Tags-up.sql'),
+  downPath: path.join(__dirname, 'sqls', 'table_Tags-down.sql'),
+})
--- tables/migrations/00000000000006-trigger-NotesCount.js
+++ tables/migrations/00000000000006-trigger-NotesCount.js
@@ -1,43 +1,7 @@
-const fs = require('fs');
 const path = require('path');
+const boilerplate = require('db-migrate-boilerplate');
 
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-  Promise = options.Promise;
-};
-
-exports.up = function(db) {
-  let filePath = path.join(__dirname, 'sqls', 'trigger-NotesCount-up.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-      if (err) return reject(err);
-      console.log('received data: ' + data);
-
-      resolve(data);
-    });
-  })
-  .then(function(data) {
-    return db.runSql(data);
-  });
-};
-
-exports.down = function(db) {
-  let filePath = path.join(__dirname, 'sqls', 'trigger-NotesCount-down.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-      if (err) return reject(err);
-      console.log('received data: ' + data);
-
-      resolve(data);
-    });
-  })
-  .then(function(data) {
-    return db.runSql(data);
-  });
-};
-
-exports._meta = {
-  'version': 1
-};
+module.exports = boilerplate ({
+  upPath: path.join(__dirname, 'sqls', 'trigger-NotesCount-up.sql'),
+  downPath: path.join(__dirname, 'sqls', 'trigger-NotesCount-down.sql'),
+})
